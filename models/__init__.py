199a7a99a19d3069c092c70a23672e0483c0cfc0
Kabir <shadmankabir@hotmail.com>
2020-11-26 22:09:03 -0800

    deleted files

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 21 21:33:13 2020
-
-@author: shadm
-"""
--- resources/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 21 21:33:26 2020
-
-@author: shadm
-"""
