65b0bc9ecea81f841f42280e82a21b028aecd418
Kabir <shadmankabir@hotmail.com>
2020-11-26 22:20:38 -0800

    First Commit, added Rest API to access items, stores, and users

---
+++ db.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 21:44:25 2020
+
+@author: shadm
+"""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
---
+++ resources/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 21 21:33:26 2020
+
+@author: shadm
+"""
