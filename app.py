184de2147f1614b0a14e1ccfbba651ab010542ca
Kabir <shadmankabir@hotmail.com>
2020-12-02 23:42:33 -0800

    Added PostgreSQL support and psycopg2 requirement

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ Created on Thu Oct 15 22:51:54 2020
 
 @author: shadm
 """
+import os
 
 from flask import Flask
 from flask_restful import Api
@@ -17,7 +18,7 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = ('kabir')
 api = Api(app)
